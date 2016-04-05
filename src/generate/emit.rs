040853489df8049058ee804e75b18670e318c81f
cjkenned <cam.j.kennedy@gmail.com>
2016-04-04 18:49:18 -0600

    Add comments to emitter.

--- src/generate/emit.rs
+++ src/generate/emit.rs
@@ -21,6 +21,8 @@ impl Emitter {
         }
     }
 
+    /// Writes a string to the current file (determined by which file the BufWriter)
+    /// is attached to.
     pub fn emit(&mut self, output: &str) {
         match write!(self.writer, "{}", output) {
             Err(error) => panic!("tank: Failed to write -  {}", Error::description(&error)),
@@ -28,18 +30,23 @@ impl Emitter {
         };
     }
 
+    /// Write a '<' symbol to file.
     pub fn left_angle_bracket(&mut self) {
         self.emit(LEFT_ANGLE_BRACKET);
     }
 
+    /// Write a '>' symbol to file.
     pub fn right_angle_bracket(&mut self) {
         self.emit(RIGHT_ANGLE_BRACKET);
     }
 
+    /// Write '=' symbol to file.
     pub fn equals(&mut self) {
         self.emit(EQUALS);
     }
 
+    /// Writes a closing tag to the file for the current element. Requires the element name
+    /// to be passed in, and then produces '</el_name>', followed by a new line.
     pub fn close_element(&mut self, tag_value: &str) {
         let mut tag = String::from(CLOSING_TAG);
         tag = tag + tag_value;
@@ -49,6 +56,8 @@ impl Emitter {
         self.emit(&tag);
     }
 
+    /// Writes a defined number of spaces to file. Used to provide correct indentation
+    /// for nested elements and contents, as well as to separate attributes.
     pub fn space(&mut self, count: usize) {
         if count == 0 {
             return;
@@ -64,12 +73,14 @@ impl Emitter {
         self.emit(&spaces);
     }
 
+    /// Writes the provided value, wrapped in double quotes. Eg. "str_val"
     pub fn string(&mut self, str_val: &str) {
         let val = QUOTE.to_owned() + str_val + QUOTE;
 
         self.emit(&val);
     }
 
+    /// Writes a new line to file.
     pub fn newline(&mut self) {
         self.emit(NEWLINE);
     }
